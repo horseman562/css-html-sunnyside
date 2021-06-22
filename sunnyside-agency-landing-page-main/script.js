cbf65562cae6337f8677f29b1c1f47ee8fa39e82
Naqiuddin <deathreaper754@gmail.com>
2021-06-23 03:37:10 +0800

    first commit

---
+++ sunnyside-agency-landing-page-main/script.js
@@ -0,0 +1,11 @@
+var toggle = document.querySelector('.icon-toggle')
+var navimo = document.querySelector('.navimo')
+ 
+ 
+
+
+
+toggle.addEventListener('click', () => {
+     console.log("lol")
+     navimo.classList.toggle("unactive")
+})
